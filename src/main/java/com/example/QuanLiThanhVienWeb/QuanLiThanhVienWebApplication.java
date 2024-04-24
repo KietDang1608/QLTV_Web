c3394cd69ba2a99aab48d550a59cae0abe932abf
KietDang1608 <kietdang1608@gmail.com>
2024-04-24 16:07:54 +0700

    first

---
+++ src/main/java/com/example/QuanLiThanhVienWeb/QuanLiThanhVienWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuanLiThanhVienWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLiThanhVienWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLiThanhVienWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuanLiThanhVienWeb/QuanLiThanhVienWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuanLiThanhVienWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLiThanhVienWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
