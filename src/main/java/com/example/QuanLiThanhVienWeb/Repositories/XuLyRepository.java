bb341a5fa33cefe7ee77276dcf12afce26c08ce5
KietDang1608 <kietdang1608@gmail.com>
2024-04-27 23:04:54 +0700

    update 1 ti

---
+++ src/main/java/com/example/QuanLiThanhVienWeb/Repositories/XuLyRepository.java
@@ -0,0 +1,10 @@
+package com.example.QuanLiThanhVienWeb.Repositories;
+
+import com.example.QuanLiThanhVienWeb.Entity.ThanhVien;
+import com.example.QuanLiThanhVienWeb.Entity.XuLy;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface XuLyRepository extends CrudRepository<XuLy,Long> {
+}
