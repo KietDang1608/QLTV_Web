27f05cf6f8171ae05ca59c331281a59103749018
MrDemoa <acquy3695741@gmail.com>
2024-05-01 23:47:14 +0700

    Hoàn thành thêm thông kê xử lý vi phạm và lọc tương ứng

--- src/main/java/com/example/QuanLiThanhVienWeb/Controller/ThongKeController.java
+++ src/main/java/com/example/QuanLiThanhVienWeb/Controller/ThongKeController.java
@@ -3,6 +3,7 @@ package com.example.QuanLiThanhVienWeb.Controller;
 import com.example.QuanLiThanhVienWeb.Entity.ThanhVien;
 import com.example.QuanLiThanhVienWeb.Entity.ThietBi;
 import com.example.QuanLiThanhVienWeb.Entity.ThongTinSD;
+import com.example.QuanLiThanhVienWeb.Entity.XuLy;
 import com.example.QuanLiThanhVienWeb.Repositories.ThanhVienRepository;
 import com.example.QuanLiThanhVienWeb.Repositories.ThietBiRepository;
 import com.example.QuanLiThanhVienWeb.Repositories.ThongTinSDRepository;
@@ -54,6 +55,7 @@ public class ThongKeController {
         m.addAttribute("cbNganh",getDSNganh());
         return "ThongKeTTSDView";
     }
+
     @GetMapping("/ThongKe/TKTB")
     public String TKThietBiMuonALL(Model m){
         ArrayList<ThongTinSD> dsmtb = getDSMTB();
@@ -68,6 +70,23 @@ public class ThongKeController {
         }
         m.addAttribute("data",listData);
         return "ThongKeTBDMView";
+    }
+    @GetMapping("/ThongKe/TKXL")
+    public String TKXuLyVPALL(Model m){
+        ArrayList<XuLy> dsvp = getDSXL();
+        ArrayList<ArrayList<String>> listData = new ArrayList<>();
+        for (XuLy xl:dsvp){
+            ArrayList<String> e = new ArrayList<>();
+            e.add(String.valueOf(xl.getMaXL()));
+            e.add(String.valueOf(xl.getMaTV()));
+            e.add(xl.getHinhThucXL());
+            e.add(String.valueOf(xl.getSoTien()));
+            e.add(xl.getNgayXL());
+            listData.add(e);
+            }
+        m.addAttribute("data",listData);
+        m.addAttribute("cbXuly",getDSHinhThucXuly());
+        return "ThongKeXLVPView";
     }
 
     @PostMapping("/ThongKe/TKTTSD/searchTKTTSD")
@@ -99,6 +118,7 @@ public class ThongKeController {
         model.addAttribute("cbnganh",nganh);
         return "ThongKeTTSDView"; // Trang hiển thị kết quả
     }
+
     @PostMapping("/ThongKe/TKTB/searchTKTB")
     public String handleSeachSubmitTB(
             @RequestParam("ngaybd") String ngaybd,
@@ -124,6 +144,32 @@ public class ThongKeController {
         return "ThongKeTBDMView"; // Trang hiển thị kết quả
     }
 
+    @PostMapping("/ThongKe/TKXL/searchTKVP")
+    public String handleSeachSubmitXL(
+            @RequestParam("ngaybd") String ngaybd,
+            @RequestParam("ngaykt") String ngaykt,
+            @RequestParam("cbxuly") String xuly,
+            Model model) {
+
+        // Xử lý giá trị được chọn
+        ArrayList<XuLy> lstFound = locxuly(ngaybd,ngaykt,xuly);
+        ArrayList<ArrayList<String>> listData = new ArrayList<>();
+        for (XuLy xl:lstFound){
+            ArrayList<String> e = new ArrayList<>();
+            e.add(String.valueOf(xl.getMaXL()));
+            e.add(String.valueOf(xl.getMaTV()));
+            e.add(xl.getHinhThucXL());
+            e.add(String.valueOf(xl.getSoTien()));
+            e.add(xl.getNgayXL());
+            listData.add(e);
+        }
+        model.addAttribute("data",listData);
+        model.addAttribute("cbXuly",getDSHinhThucXuly());
+        model.addAttribute("ngaybd",ngaybd);
+        model.addAttribute("ngaykt",ngaykt);
+        model.addAttribute("cbxuly",xuly);
+        return "ThongKeXLVPView"; // Trang hiển thị kết quả
+    }
     private ArrayList<ThongTinSD> getDSTVByStart(String startDateTime){
         //Datetime: yyyy-mm-ddThh:mm
 
@@ -143,6 +189,7 @@ public class ThongKeController {
         }
         return lstFound;
     }
+
     private ArrayList<ThongTinSD> getDSTBByStart(String startDateTime){
         //Datetime: yyyy-mm-ddThh:mm
 
@@ -162,6 +209,26 @@ public class ThongKeController {
         }
         return lstFound;
     }
+    private ArrayList<XuLy> getDSXLByStart(String startDateTime){
+        //Datetime: yyyy-mm-ddThh:mm
+
+        ArrayList<XuLy> lstFound = new ArrayList<>();
+        LocalDateTime dateTime = LocalDateTime.parse(startDateTime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = dateTime.format(formatter);
+        LocalDateTime datetimeStart = LocalDateTime.parse(formattedDateTime,formatter);
+
+        for (XuLy xl: xlRe.findAll()){
+            if (xl.getTrangThaiXL() == 1) {
+                LocalDateTime dt = LocalDateTime.parse(xl.getNgayXL(),formatter);
+                if (dt.isEqual(datetimeStart) || dt.isAfter(datetimeStart)) {
+                    lstFound.add(xl);
+                }
+            }
+        }
+        return lstFound;
+    }
+
     private ArrayList<ThongTinSD> getDSTVByEnd(String endDateTime){
         ArrayList<ThongTinSD> lstFound = new ArrayList<>();
         LocalDateTime dateTime = LocalDateTime.parse(endDateTime);
@@ -178,6 +245,7 @@ public class ThongKeController {
         }
         return lstFound;
     }
+
     private ArrayList<ThongTinSD> getDSTBByEnd(String endDateTime){
         ArrayList<ThongTinSD> lstFound = new ArrayList<>();
         LocalDateTime dateTime = LocalDateTime.parse(endDateTime);
@@ -194,6 +262,24 @@ public class ThongKeController {
         }
         return lstFound;
     }
+
+    private ArrayList<XuLy> getDSXLByEnd(String endDateTime){
+        ArrayList<XuLy> lstFound = new ArrayList<>();
+        LocalDateTime dateTime = LocalDateTime.parse(endDateTime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = dateTime.format(formatter);
+        LocalDateTime datetimeEnd = LocalDateTime.parse(formattedDateTime,formatter);
+        for (XuLy xl: xlRe.findAll()){
+            if (xl.getTrangThaiXL() == 1) {
+                LocalDateTime dt = LocalDateTime.parse(xl.getNgayXL(),formatter);
+                if (dt.isEqual(datetimeEnd) || dt.isBefore(datetimeEnd)) {
+                    lstFound.add(xl);
+                }
+            }
+        }
+        return lstFound;
+    }
+
     private ArrayList<ThongTinSD> getDSVAO(){
         ArrayList<ThongTinSD> lstFound = new ArrayList<>();
         for (ThongTinSD tt: ttRe.findAll()){
@@ -213,6 +299,15 @@ public class ThongKeController {
         return  lstFound;
     }
 
+    private ArrayList<XuLy> getDSXL(){
+        ArrayList<XuLy> lstFound = new ArrayList<>();
+        for (XuLy xl: xlRe.findAll()){
+            if (xl.getTrangThaiXL() == 1){
+                lstFound.add(xl);
+            }
+        }
+        return lstFound;
+    }
     private ArrayList<ThongTinSD> getDSThanhVienByKhoa(String khoa){
         ArrayList<ThongTinSD> lstFound = new ArrayList<>();
         for (ThongTinSD tt: ttRe.findAll()){
@@ -226,6 +321,20 @@ public class ThongKeController {
         }
         return lstFound;
     }
+    private ArrayList<XuLy> getDSXuLyByHinhThucXuLy(String xuly){
+        ArrayList<XuLy> lstFound = new ArrayList<>();
+        for (XuLy xl: xlRe.findAll()){
+            Optional<XuLy> optionalXuLy = xlRe.findById((long) xl.getMaXL());
+            XuLy xly = optionalXuLy.get();
+            if (xl.getTrangThaiXL() == 1){
+                if (xly.getHinhThucXL().contains(xuly)) {
+                    lstFound.add(xl);
+                }
+            }
+        }
+        return lstFound;
+    }
+
     private ArrayList<ThongTinSD> getDSTVBYNganh(String nganh){
         ArrayList<ThongTinSD> lstFound = new ArrayList<>();
         for (ThongTinSD tt: ttRe.findAll()){
@@ -259,6 +368,18 @@ public class ThongKeController {
         // Convert the HashSet to an ArrayList
         return new ArrayList<>(uniqueKhoasSet);
     }
+
+    private ArrayList<String> getDSHinhThucXuly(){
+        HashSet<String> uniqueXulysSet = new HashSet<>();
+        uniqueXulysSet.add("All");
+        // Iterate over the XuLy objects and add their xuly values to the uniqueXulys list
+        for (XuLy xl: xlRe.findAll()) {
+            uniqueXulysSet.add(xl.getHinhThucXL());
+        }
+        // Convert the HashSet to an ArrayList
+        return new ArrayList<>(uniqueXulysSet);
+    }
+
     private String getNameByID(int id){
         for (ThanhVien tt : tvRe.findAll()){
             if (tt.getMaTV() == id)
@@ -309,6 +430,19 @@ public class ThongKeController {
         // Chuyển đổi HashSet thành ArrayList và trả về
         return new ArrayList<>(commonElementsSet);
     }
+    private ArrayList<XuLy> layPhanGiaoXL(ArrayList<XuLy>... lists) {
+        // Khởi tạo một HashSet để chứa các phần tử chung
+        HashSet<XuLy> commonElementsSet = new HashSet<>(lists[0]);
+        // Duyệt qua từng ArrayList trong danh sách đầu vào
+        for (int i = 1; i < lists.length; i++) {
+            // Tạo một HashSet tạm thời chứa phần tử của ArrayList hiện tại
+            HashSet<XuLy> currentListSet = new HashSet<>(lists[i]);
+            // Giữ lại các phần tử chung với commonElementsSet
+            commonElementsSet.retainAll(currentListSet);
+        }
+        // Chuyển đổi HashSet thành ArrayList và trả về
+        return new ArrayList<>(commonElementsSet);
+    }
     public ArrayList<ThongTinSD> loc(String ngaybd, String ngaykt, String khoa, String nganh){
         ArrayList<ThongTinSD> theoNgayBD = getDSVAO();
         ArrayList<ThongTinSD> theoNgayKT =  getDSVAO();
@@ -335,6 +469,7 @@ public class ThongKeController {
         return layPhanGiao(theoNgayBD,theoNgayKT,theoKhoa,theoNganh);
 
     }
+
     public ArrayList<ThongTinSD> loctheongaymuon(String ngaybd, String ngaykt){
         ArrayList<ThongTinSD> theoNgayBD = getDSMTB();
         ArrayList<ThongTinSD> theoNgayKT =  getDSMTB();
@@ -348,5 +483,24 @@ public class ThongKeController {
         }
 //        for (ThongTinSD tt : theoNganh) System.out.println(tt.toString());
         return layPhanGiao(theoNgayBD,theoNgayKT);
+    }
+
+    public ArrayList<XuLy> locxuly(String ngaybd, String ngaykt,String xuly){
+        ArrayList<XuLy> theoNgayBD = getDSXL();
+        ArrayList<XuLy> theoNgayKT = getDSXL();
+        ArrayList<XuLy> theoXuly = getDSXL();
+        if (!ngaybd.equals("")){
+            theoNgayBD = new ArrayList<>();
+            theoNgayBD = getDSXLByStart(ngaybd);
+        }
+        if (!ngaykt.equals("")){
+            theoNgayKT = new ArrayList<>();
+            theoNgayKT = getDSXLByEnd(ngaykt);
+        }
+        if (!xuly.equals("")){
+            theoXuly = new ArrayList<>();
+            theoXuly = getDSXuLyByHinhThucXuLy(xuly);
+        }
+        return layPhanGiaoXL(theoNgayBD,theoNgayKT,theoXuly);
     }
 }
