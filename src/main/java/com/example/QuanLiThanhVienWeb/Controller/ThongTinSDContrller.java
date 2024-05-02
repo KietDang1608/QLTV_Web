969dd86d5d18cdbfc1699787b5bcd33e446dbf96
Mackeddy <duyphan1305@gmail.com>
2024-05-02 16:31:18 +0700

    Them ham muon

--- src/main/java/com/example/QuanLiThanhVienWeb/Controller/ThongTinSDContrller.java
+++ src/main/java/com/example/QuanLiThanhVienWeb/Controller/ThongTinSDContrller.java
@@ -6,12 +6,16 @@ import com.example.QuanLiThanhVienWeb.Entity.ThongTinSD;
 import com.example.QuanLiThanhVienWeb.Repositories.ThietBiRepository;
 import com.example.QuanLiThanhVienWeb.Repositories.ThongTinSDRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import java.util.ArrayList;
@@ -30,7 +34,7 @@ public class ThongTinSDContrller {
     @Autowired
     private ScheduledTasks service;
 
-    private static ArrayList<ThongTinSDContrller> list = new ArrayList();
+    private static ArrayList<ThongTinSD> listTT = new ArrayList();
     private static ArrayList<ThietBi> listTB = new ArrayList();
 
     @RequestMapping("/userThietBi")
@@ -55,15 +59,10 @@ public class ThongTinSDContrller {
     @RequestParam("maTB") int maTB) {
 
         //Kiểm tra thiết bị có đang cho mượn hoặc đặt chỗ không
-        Boolean inTTSD = ttsdRepository.existsByMaTB(maTB);
-        boolean isToday = ttsdRepository.existsByMaTBAndNgayDatCho(maTB);
-        System.out.println("nguoi dat cho " + inTTSD);
-        System.out.println("nguoi dat cho " + isToday);
-        if (!inTTSD || inTTSD & !isToday) { //thiết bị chưa được mượn bao giờ hoặc đã được mượn và hiện đang trống
+        if (!isDatChoByMaTB(maTB)){
+            System.out.println("Thiết bị có thể đặt chỗ" + isDatChoByMaTB(maTB));
             //Lấy thời gian hiện tại
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date date = new Date();
-            ttsd.setTGDatcho(formatter.format(date));
+            ttsd.setTGDatcho(takeCurrentDay());
             String tg_datcho = ttsd.getTGDatcho();
             System.out.println(tg_datcho);
 
@@ -82,7 +81,26 @@ public class ThongTinSDContrller {
         }
         return "userThietbiView";
     }
-    
+
+
+
+    //Hàm mượn thiết bị
+    @RequestMapping(value = {"muon"}, method = RequestMethod.POST)
+    public String saveMuon(Model model, @ModelAttribute("thongtinsd") ThongTinSD ttsd,
+    @RequestParam("maTB") int maTB, @RequestParam("maTV") int maTV) {
+        if (checkMuon(maTV, maTB)) { //thiết bị chưa được mượn bao giờ hoặc đã được mượn và hiện đang trống
+            ttsd.setTgMuon(takeCurrentDay());
+            ttsd = new ThongTinSD(maTB, ttsd.getTgMuon());
+            ttsdRepository.save(ttsd);
+        } else {
+            System.out.println("khong the muon luc nay");
+        }
+        return "userThietbiView";
+    }
+
+    //Hàm trả thiết bị
+
+
     @PostMapping("/QLDatCho/searchTBbyName")
     public String handleSeachSubmit(
         @RequestParam("search") String search,
@@ -107,5 +125,49 @@ public class ThongTinSDContrller {
         model.addAttribute("data",lstFound);
         
         return "userThietbiView"; // Trang hiển thị kết quả
+    }
+
+
+    public Boolean isDatChoByMaTB(Integer maTB) {
+        for (ThongTinSD tt : ttsdRepository.findAll()) {
+            // Nếu có thiết bị trong db
+            if (tt.getMaTB() == maTB) {
+                return tt.getTGDatcho() != null || tt.getTgMuon() != null;
+            }
+        }
+        // Nếu không tìm thấy mã thiết bị
+        return false;
+    }
+
+    public boolean checkMuon(Integer maTV, Integer maTB) {
+        for (ThongTinSD tt : ttsdRepository.findAll()) {
+            if (tt.getMaTB() == maTB){
+                //mình đang đặt chỗ
+                if (tt.getMaTV() == maTV && tt.getTGDatcho() != null) {
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    LocalDateTime tg_datcho = LocalDateTime.parse(tt.getTgMuon(), formatter);
+                    if (tg_datcho.isAfter(LocalDateTime.now().minusHours(1)))
+                        return true;
+                } else if (isDatChoByMaTB(maTB) || (tt.getTgMuon() != null && tt.getTgTra() == null)) {
+                    // có ai đang đặt chỗ hoặc đang mượn
+                    // thì ko được mượn
+                    return false;
+                }
+            }
+        }
+        //Thiết bị đang trống có thể mượn
+        return true;
+    }
+
+
+//    public DatetoTime(){
+//        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+//        Date date = new Date();
+//        return formatter.format(date);
+//    }
+    public String takeCurrentDay(){
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return currentDate.format(formatter);
     }
 }
